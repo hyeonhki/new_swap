de5b6428f4ad330976df1fd34c6c94b8330e41e9
kuh7753 <kuh7753@naver.com>
2022-01-30 21:05:55 +0900

    clear

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hyeonhki <hyeonhki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 11:25:59 by hyeonhki          #+#    #+#             */
-/*   Updated: 2022/01/30 20:41:26 by hyeonhki         ###   ########.fr       */
+/*   Updated: 2022/01/30 21:05:38 by hyeonhki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,34 @@ void	prgm_init(t_program *prgm)
 	prgm->nbrneg = 0;
 	prgm->range = 0;
 	prgm->pm_cnt = 0;
+}
+
+int	is_letter(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{	
+		if (!(str[i] >= '0' && str[i] <= '9'))
+		{
+			if (!(str[i] == '-' || str[i] == '+'))
+				return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	letter_check(int nb, char **arg)
+{
+	while (nb > 1)
+	{
+		if (is_letter(arg[nb - 1]) == 1)
+			return (1);
+		nb--;
+	}
+	return (0);
 }
 
 void	check_stack(t_element *a, t_element *b)
@@ -60,6 +88,8 @@ int	main(int nb, char **arg)
 		return (0);
 	swap.arr = (int *)malloc((nb - 1) * sizeof(int));
 	swap.flag = 0;
+	if (letter_check(nb, arg) == 1)
+		return (msg_error("Error\n"));
 	prgm_init(&prgm);
 	a = stack_init(nb, arg, &prgm);
 	if (error_check(&prgm, nb, a) == 1)
